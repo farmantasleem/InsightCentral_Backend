f9aaca03fec96970d7ed4de25debac3220fe8e77
unknown <iamfarmanmalik@gmail.com>
2022-12-22 23:08:53 +0530

    file updated

--- models/user.model.js
+++ models/user.model.js
@@ -2,11 +2,12 @@ const mongoose=require("mongoose");
 
 const userSchema=mongoose.Schema({
     name:{type:String,required:true},
-    bio:{type:String},
-    img:{type:String},
+    bio:{type:String,default:""},
+    img:{type:String,default:"https://www.mymasala.in/wp-content/uploads/2022/05/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"},
     email:{type:String,required:true},
     password:{type:String,required:true},
     role:{type:String,default:"user"},
+
     
 })
 
